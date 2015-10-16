8a98f3d4dde590e8348d27e477e3bb0f2f097c41
bumaklion <olle.wahlstrom@centsoft.se>
2015-10-16 15:46:30 +0200

    har lagt in logik i factory och har två separata controllers för "listan" och "edit-panelen". Också ett enter-tab script..

--- app/app.js
+++ app/app.js
@@ -2,11 +2,40 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
+])
+
+
+    .controller('LoadScripts', ['$scope', function ($scope) {
+        // Catch the keydown for the entire document
+
+        angular.element(document).ready(function () {
+            $('body').on('keydown', 'input, select', function (e) {
+                var self = $(this)
+                    , form = self.parents('form:eq(0)')
+                    , focusable
+                    , next
+                    ;
+                if (e.keyCode == 13) {
+                    focusable = form.find('input,a,select,button,textarea').filter(':visible');
+                    next = focusable.eq(focusable.index(this) + 1);
+                    if (next.length) {
+                        next.focus();
+                    } else {
+                        form.submit();
+                    }
+                    return false;
+                }
+            });
+
+        });
+
+    }]).
+
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/view1'});
+    }]
+);
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -6,68 +6,127 @@ angular.module('myApp.view1', ['ngRoute'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/view1', {
             templateUrl: 'view1/view1.html',
-            controller: 'View1Ctrl',
-            controllerAs: 'mittAlias'
+            controller: 'KvittoListController'
         });
     }])
 
+    /*kontroller för listan*/
+    .controller('KvittoListController', ['$scope', 'Kvitto', function ($scope, Kvitto) {
+        $scope.Kvitto = Kvitto;
+    }])
 
-    .directive('recipeBox', function () {
+    /*factory som innehåller delad funtionalitet*/
+    .factory("Kvitto", function KvittoFactory() {
+        var allaKvitton = reciepts;
+        var kvittoToEdit;
+        return {
+            all: function
+                () {
+                return allaKvitton;
+            }
+            ,
+
+            remove: function (kvittoToRemove) {
+                allaKvitton.splice(allaKvitton.indexOf(kvittoToRemove), 1);
+            }
+            ,
+
+            getKvittoToEdit: function () {
+                return kvittoToEdit;
+            }
+            ,
+
+            editThis: function (newKvittoToEdit) {
+                if (newKvittoToEdit) {
+                    if (!newKvittoToEdit.rader)
+                        newKvittoToEdit.rader = {};
+                    if (newKvittoToEdit.rader.length == 0)
+                        newKvittoToEdit.rader.push({});
+                }
+                kvittoToEdit = newKvittoToEdit;
+            }
+        }
+            ;
+    })
+
+    /*directive för recipe box*/
+    .
+    directive('recipeBox', function () {
         return {
             restrict: 'E',
             templateUrl: 'view1/recipe-box.html'
         };
     })
 
-    .directive('editRecipe', function(){
+    /*directive för edit, eget scope och kontroller!*/
+    .directive('editRecipe', function () {
         return {
             restrict: 'E',
-            templateUrl: 'view1/edit-recipe.html'
+            templateUrl: 'view1/edit-recipe.html',
+            controller: ['$scope', 'Kvitto', function ($scope, Kvitto) {
+                $scope.Kvitto = Kvitto;
+
+                $scope.newRow = function () {
+                    Kvitto.getKvittoToEdit().rader.push({});
+                }
+
+                $scope.formSubmit = function () {
+                    Kvitto.editThis(null);
+                }
+
+            }],
+            controllerAs: 'EditController',
+            scope: {}
         };
     })
-
-
-    .controller('View1Ctrl', [function () {
-        this.kvitton = reciepts;
-        this.currentKvittoToEdit;
-
-        var controller = this;
-
-        this.taBort = function (kvittoAttTaBort) {
-            controller.kvitton.splice(controller.kvitton.indexOf(kvittoAttTaBort), 1);
-        };
-
-        this.editThis = function (kvittoToEdit) {
-            controller.currentKvittoToEdit = kvittoToEdit;
-        };
-
-    }])
 
 var reciepts = [
     {
         datum: "2015-06-14",
         anteckning: "Representationsmiddag med säljarna på företaget som vi ska köpa.",
-        bild: "view1/k1.jpg"
+        bild: "view1/k1.jpg",
+        totalBelopp: 8000,
+        rader: [
+            {
+                belopp: 600.37,
+                konto: "4010",
+                kostnadstyp: "kontorsmaterial",
+                dim1: null
+            },
+            {
+                belopp: 7399.63,
+                konto: "5020",
+                kostnadstyp: null,
+                dim1: null
+            }
+        ]
     },
     {
         datum: "2015-10-14",
         anteckning: "Maja, Simon, Pelle.",
-        bild: "view1/k2.jpg"
+        bild: "view1/k2.jpg",
+        totalBelopp: null,
+        rader: []
     },
     {
         datum: "2015-06-14",
         anteckning: "Grejer till mässan..",
-        bild: "view1/k3.jpg"
+        bild: "view1/k3.jpg",
+        totalBelopp: null,
+        rader: []
     },
     {
         datum: "2015-06-14",
         anteckning: "Avskedsfika till Börje",
-        bild: "view1/k4.jpg"
+        bild: "view1/k4.jpg",
+        totalBelopp: null,
+        rader: []
     },
-
     {
         datum: "2015-06-14",
         anteckning: "",
-        bild: "view1/k5.jpg"
+        bild: "view1/k5.jpg",
+        totalBelopp: null,
+        rader: []
     }
 ];
